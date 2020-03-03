9896a36e374acbfdaa18115adc196e3d456696a2
Alexander Strachan <afms135@gmail.com>
2020-03-03 00:20:47 +0000

    Disable copy constructor and copy assigment operator from effect manager.

--- Software/effect_manager.h
+++ Software/effect_manager.h
@@ -10,6 +10,9 @@ public:
 
 	std::unique_ptr<effect> operator()(int idx);
 	int size();
+
+	effect_manager(const effect_manager& other) = delete;
+	effect_manager& operator=(const effect_manager& other) = delete;
 };
 
 #endif /*EFFECTMANAGER_H*/
