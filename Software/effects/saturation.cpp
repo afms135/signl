84861e0eaea6132167afee337bb859478d735913
robnorr <robert.norris4@gmail.com>
2020-03-23 15:28:17 +0000

    Updated saturation effect - new waveshaper and added level and gain parameters. Also rename.

--- Software/effects/saturation.cpp
+++ Software/effects/saturation.cpp
@@ -4,7 +4,7 @@
 class plugin : public effect
 {
 public:
-	plugin(unsigned int rate) : thresh(0.5), drive(0.2)
+	plugin(unsigned int rate) : gain(2.0), gain_raw(0.0), drive(1.44), drive_raw(0.2), level(1.0)
 	{
 	}
 
@@ -12,45 +12,50 @@ public:
 	{
 		float abs_in = std::abs(in);
 
-		if (in < thresh)
-		{
-			return in;
-		}
-		else
-		{
-			float ret = (thresh + (abs_in - thresh) / (1 + pow( (abs_in - thresh ) / (1 - thresh ), drive*10.0)));
-			return (in > 0) ? ret : -ret;
-		}
+		return (in*(abs_in + drive)/(pow(in,2) + (drive-1)*abs_in + 1) ) * level;
 	}
 
 	void paramset(param p, float v) override
 	{
 		if(p == PARAM_A)
-			thresh = v;
+		{
+			gain_raw = v;
+			gain = (v*2)+2;
+			drive = pow(drive_raw+1,gain);
+		}
 		else if(p == PARAM_B)
-			drive = v;
+		{
+			drive_raw = v;
+			drive = pow(v+1,gain);
+		}
+		else if(p == PARAM_D)
+			level = v;
 	}
 
 	std::string name() override
 	{
-		return "Tape Saturation";
+		return "Saturation";
 	}
 
 	std::string paramname(param p) override
 	{
 		if(p == PARAM_A)
-			return "Thresh";
-		else if(p == PARAM_B)
+			return "Gain";
+		else if (p == PARAM_B)
 			return "Drive";
+		else if (p == PARAM_D)
+			return "Level";
 		return "";
 	}
 
 	float paramval(param p) override
 	{
 		if(p == PARAM_A)
-			return thresh;
+			return gain_raw;
 		else if(p == PARAM_B)
-			return drive;
+			return drive_raw;
+		else if(p == PARAM_D)
+			return level;
 		return -1;
 	}
 
@@ -60,8 +65,11 @@ public:
 	}
 
 private:
-	float thresh;
+	float gain;
+	float gain_raw;
 	float drive;
+	float drive_raw;
+	float level;
 };
 
 PLUGIN_API
