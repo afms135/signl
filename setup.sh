4c3be7cd659c6a00a6d3d3aecd0de0d0cb516fbf
Alexander Strachan <afms135@gmail.com>
2020-03-27 01:36:39 +0000

    Install needed prerequisites.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,11 @@ if ![ $(id -u) = 0 ]; then
 	echo "Script needs to be run as root!"
 	exit 1
 fi
+
+#Installing required prerequisites
+echo "Installing required prerequisites..."
+apt-get update
+apt-get -y install jackd2 libjack-jackd2-dev git
+if [ $? -ne 0 ]; then
+	echo "apt-get failed (Are you connected to the internet?)"
+fi
