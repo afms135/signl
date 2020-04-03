39055e9c779a45abfc99fff07abbf3ef5c8b69c5
robnorr <robert.norris4@gmail.com>
2020-04-03 11:19:26 +0100

    Added the convolve function, but it doesn't seem to be affecting the output in any way.

--- Software/effects/conv.cpp
+++ Software/effects/conv.cpp
@@ -1,7 +1,8 @@
 #include "effect.h"
 #include <cmath>
-#include <thread>
-#include <algorithm>
+#include <thread> // std::thread()
+#include <algorithm> // std::copy()
+#include <vector> // std::vector()
 
 /*
 This should a power of 2 no larger than 128 @ sample_rate = 48kHz:
@@ -19,28 +20,28 @@ This should a power of 2 no larger than 128 @ sample_rate = 48kHz:
 //         before we can process it. Would be back down to the above times
 //         if we pass a buffer straight to the plugin () from jack_client::process
 
-#define CONV_BUFFER_LENGTH 128
+#define CONV_BUFFER_LENGTH 256
 
 class plugin : public effect
 {
 public:
-	plugin(unsigned int rate)
+	plugin(unsigned int rate) : kernel(128,1.0f)
 	{
 	}
 
 	float operator()(float in) override
 	{
-		in_buf[in_buf_idx++] = in;
-		if (in_buf_idx == CONV_BUFFER_LENGTH)
+		in_buf[buf_idx++] = in;
+		if (buf_idx == CONV_BUFFER_LENGTH)
 		{
-			in_buf_idx = 0;
+			buf_idx = 0;
 			std::copy(conv_buf, conv_buf+CONV_BUFFER_LENGTH, out_buf);
 			std::copy(in_buf, in_buf+CONV_BUFFER_LENGTH, conv_buf);
-			std::thread t(convolve,conv_buf);
+			std::thread t(&plugin::convolve,this,conv_buf);
 			t.detach();
 		}
 
-		return out_buf[in_buf_idx];
+		return out_buf[buf_idx];
 	}
 
 	void paramset(param p, float v) override
@@ -68,16 +69,30 @@ public:
 	}
 
 private:
-	static void convolve(float* conv_buf)
+	void convolve(float* conv_buf)
 	{
+		std::vector<float> input(conv_buf, conv_buf + CONV_BUFFER_LENGTH);
+
+		unsigned int paddedLength = input.size() + kernel.size() - 1;
+		std::vector<float> convolved(paddedLength); //zeros
+
+		for(unsigned int i=0; i<paddedLength; i++) //index into 'convolved' vector
+		{
+			int startk = (i >= input.size()) ? i - input.size() + 1 : 0;
+			int endk = (i < kernel.size()) ? i : kernel.size() - 1;
+
+			for( int kernel_idx = startk; kernel_idx < endk; kernel_idx++ )
+				convolved[i] += kernel[kernel_idx]*input[i-kernel_idx];
+		}
+		conv_buf = &convolved[0];
 	}
 
 	float in_buf[CONV_BUFFER_LENGTH] = {0.0};
 	float conv_buf[CONV_BUFFER_LENGTH] = {0.0};
 	float out_buf[CONV_BUFFER_LENGTH] = {0.0};
-	unsigned int in_buf_idx = 0;
+	unsigned int buf_idx = 0;
 
-//	std::thread t;
+	std::vector<float> kernel;
 };
 
 PLUGIN_API
