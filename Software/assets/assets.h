d4a9a64133b97ecec160818751f0b16c47c5207b
Alexander Strachan <afms135@gmail.com>
2020-04-05 01:50:08 +0100

    Add documentation for Sprite struct.

--- Software/assets/assets.h
+++ Software/assets/assets.h
@@ -3,9 +3,32 @@
 #include <vector>
 #include <cstdint>
 
+/**
+ * \brief Sprite definition
+ *
+ * This structure defines a 1 bit-per-pixel sprite suitable for displaying
+ * on a black-and-white lcd.
+ *
+ * Example:
+ * \code
+ * Sprite square =
+ * {
+ * 	{
+ * 		1,1,1,1,1,
+ * 		1,0,0,0,1,
+ * 		1,0,0,0,1,
+ * 		1,0,0,0,1,
+ * 		1,1,1,1,1
+ * 	},
+ * 	5
+ * };
+ * \endcode
+ */
 struct Sprite
 {
+	///Vector of bits in sprite, true for set, false for clear.
 	const std::vector<bool> art;
+	///Width of sprite in pixels.
 	uint8_t width;
 };
 
