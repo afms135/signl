4c4523288fb17ff5f07d22686edf18f3ee523c6f
Simon Ridley <simonridley97@gmail.com>
2020-04-09 12:40:11 +0100

    Added fade option, changed icon.

--- Software/effects/tremolo.cpp
+++ Software/effects/tremolo.cpp
@@ -3,7 +3,7 @@
 class plugin : public effect
 {
 public:
-	plugin(unsigned int rate) : index(0), noofsamples(13200), period(0.5), cut(1)
+	plugin(unsigned int rate) : index(0), noofsamples(13200), period(0.5), cut(1), fade(0.1)
 	{
 	}
 
@@ -11,9 +11,15 @@ public:
 	{
 		if(index++ > noofsamples)
 		{
+			return in * (1-((cut / (noofsamples * fade)) * index));
+		} 
+		else if(index++ > noofsamples * (1+fade))
+		{
 			index %= noofsamples * 2; 
 			return in * (1-cut);
-		}
+		} 
+		else if(index++ < noofsamples * fade)
+			return in * ((cut / (noofsamples * fade)) * index);
 		
 		return in;
 	}
@@ -26,6 +32,9 @@ public:
 			noofsamples = (period * 24000) + 2400;
 		} else if(p == PARAM_B)
 			cut = v;
+		else if(p == PARAM_C)
+			fade = v;
+
 
 	}
 
@@ -40,6 +49,8 @@ public:
 			return "Period";
 		else if(p == PARAM_B)
 			return "Cut";
+		else if(p == PARAM_C)
+			return "Fade";
 		return "";
 	}
 
@@ -49,12 +60,14 @@ public:
 			return period;
 		else if(p == PARAM_B)
 			return cut;
+		else if(p == PARAM_C)
+			return fade;
 		return -1;
 	}
 
 	gui_icon icon() override
 	{
-		return ICON_AMP;
+		return ICON_PEDAL;
 	}
 
 private:
@@ -63,6 +76,7 @@ private:
 	unsigned int noofsamples;
 	float period;
 	float cut;
+	float fade;
 };
 
 PLUGIN_API
