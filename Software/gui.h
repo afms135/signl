e278b5d732b4cbf9f2bf02e5ccd65668958ef879
Alexander Strachan <afms135@gmail.com>
2020-04-11 04:55:37 +0100

    Add documentation for gui class private members.

--- Software/gui.h
+++ Software/gui.h
@@ -18,9 +18,52 @@ public:
 	void level_view(float in_level, float out_level, jack_client::sample_t sample_array[7][BUFFER_LENGTH]);
 
 private:
+	/**
+	 * \brief Draw Sprite on lcd.
+	 *
+	 * Draw a Sprite object on the lcd, overwrites pixels in lcd framebuffer.
+	 *
+	 * \param sprite A Sprite object to draw on the lcd.
+	 * \param x_origin Horizontal co-ordinate (0 <= x_origin < #lcd.width).
+	 * \param y_origin Vertical co-ordinate (0 <= y_origin < #lcd.height).
+	 * \param inv True to invert Sprite; False for standard (optional: defaults to false).
+	 */
 	void putsprite(Sprite sprite, unsigned int x_origin, unsigned int y_origin, bool inv = false);
+
+	/**
+	 * \brief Draw character on lcd.
+	 *
+	 * Draw a ASCII character on the lcd using Sprites defined in \c assets/font.cpp.
+	 *
+	 * \note Supported characters are A-Z 0-9 . , ; : \ ! " ? - + =
+	 *
+	 * \param ch ASCII character to draw.
+	 * \param x_origin Horizontal co-ordinate (0 <= x_origin < #lcd.width).
+	 * \param y_origin Vertical co-ordinate (0 <= y_origin < #lcd.height).
+	 */
 	void putchar(char ch, unsigned int x_origin, unsigned int y_origin);
+
+	/**
+	 * \brief Draw string on lcd.
+	 *
+	 * Draw a ASCII encoded string on the lcd using the font defined in \c assets/font.cpp.
+	 *
+	 * \param str String to draw.
+	 * \param x_origin Horizontal co-ordinate (0 <= x_origin < #lcd.width).
+	 * \param y_origin Vertical co-ordinate (0 <= y_origin < #lcd.height).
+	 */
 	void putstring(std::string str, unsigned int x_origin, unsigned int y_origin);
+
+	/**
+	 * \brief Draw rectangle outline on lcd.
+	 *
+	 * Draw a rectange outline on lcd, inverts pixels currently in framebuffer.
+	 *
+	 * \param x_origin Horizontal co-ordinate (0 <= x_origin < #lcd.width).
+	 * \param y_origin Vertical co-ordinate (0 <= y_origin < #lcd.height).
+	 * \param width Width of rectangle in pixels.
+	 * \param height Hight of rectangle in pixels.
+	 */
 	void putrect(unsigned int x_origin, unsigned int y_origin, unsigned int width, unsigned int height);
 };
 
