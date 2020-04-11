6b13b83031dfecef8989016692c2443c7585f8aa
Alexander Strachan <afms135@gmail.com>
2020-04-11 04:26:04 +0100

    Add coloured output to script.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 #Check if script is running as root
 if [ "$EUID" -ne 0 ]; then
-	echo "Script needs to be run as root!"
+	echo -e "${RED}Script needs to be run as root!${NC}"
 	exit 1
 fi
 
 #Installing required prerequisites
-echo "Installing required prerequisites..."
+echo -e "${RED}Installing required prerequisites...${NC}"
 apt-get update
 apt-get -y install jackd2 libjack-jackd2-dev git
 if [ $? -ne 0 ]; then
-	echo "apt-get failed (Are you connected to the internet?)"
+	echo -e "${RED}apt-get failed (Are you connected to the internet?)${NC}"
 fi
 
 #Sound card/HW setup
-echo "Configuring Hardware..."
-echo "Disabling internal sound card..."
+echo -e "${RED}Configuring Hardware...${NC}"
+echo -e "${RED}Disabling internal sound card...${NC}"
 sed -i "s:dtparam=audio:#dtparam=audio:" /boot/config.txt
-echo "Enable WM8731 sound card..."
+echo -e "${RED}Enable WM8731 sound card...${NC}"
 if [ $(grep -c audioinjector-wm8731-audio /boot/config.txt) -eq "0" ]; then
 	echo "dtoverlay=audioinjector-wm8731-audio" >> /boot/config.txt
 fi
